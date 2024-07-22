166b09b94dda592dd1beba55bbdce56d841a46cb
mamebassine <mamebassine06@gmail.com>
2024-07-22 22:37:30 +0000

    Premier commit

---
+++ exercices1.js
@@ -0,0 +1,10 @@
+// Déclaration des variables
+var nom = 'MOUSSA';
+var age = 30;
+// Fonction pour afficher les valeurs
+function afficherInformations(nom, age) {
+    console.log("Nom: ".concat(nom));
+    console.log("\u00C2ge: ".concat(age));
+}
+// Appel de la fonction pour afficher les valeurs
+afficherInformations(nom, age);
---
+++ exercices1.ts
@@ -0,0 +1,12 @@
+// Déclaration des variables
+let nom: string = 'MOUSSA';
+let age: number = 30;
+
+// Fonction pour afficher les valeurs
+function afficherInformations(nom: string, age: number): void {
+  console.log(`Nom: ${nom}`);
+  console.log(`Âge: ${age}`);
+}
+
+// Appel de la fonction pour afficher les valeurs
+afficherInformations(nom, age);
